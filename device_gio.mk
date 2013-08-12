d9bb2985528c6c8b683287f1eba04ab4a0f03554
erikcas <erikcas1972@gmail.com>
2013-08-12 17:57:38 +0200

    build with 2.6.35.7 kernel && old style athk6

--- device_gio.mk
+++ device_gio.mk
@@ -19,9 +19,9 @@ PRODUCT_PACKAGES += \
     wmiconfig
 
 ## Build the 3.0.x Kernel
-BUILD_WITH_30X_KERNEL := true
+# BUILD_WITH_30X_KERNEL := true
 
-BOARD_WLAN_DEVICE := ath6kl_compat
+# BOARD_WLAN_DEVICE := ath6kl_compat
 
 ## Ramdisk
 PRODUCT_COPY_FILES += \
